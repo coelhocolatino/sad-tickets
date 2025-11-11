81a46f3c3ea83d8d0e3747ccdafd5e26b03240d3
coelhocolatino <colatino.ventas.enlinea@gmail.com>
2025-11-11 01:25:38 +0100

    Comment out no-cors mode in fetch request

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ formEl.addEventListener("submit", async (e) => {
   try {
     await fetch(BACKEND_URL, {
       method: "POST",
-      mode: "no-cors",
+     // mode: "no-cors",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
     });
